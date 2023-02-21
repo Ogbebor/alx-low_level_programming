3c718f9d9048fc43f8b89410285117bc4914ed33
myusername <you@example.com>
2023-02-21 13:39:48 -0800

    upd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - print a line of code using puts
+ *main- Serves as the entry point for the program
  *
- * Return: 0
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    Return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,1 @@
-0-preprocessor
-0. Preprocessor
+myhelloworldreadme
